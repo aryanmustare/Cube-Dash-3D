7531d801c50160501de456b9f753025c06fa63ad
aryanmustare <aryanm654321@gmail.com>
2025-01-12 00:57:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,685 @@
+
+Detector = {
+
+	canvas: !! window.CanvasRenderingContext2D,
+	webgl: ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )(),
+	workers: !! window.Worker,
+	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+
+	getWebGLErrorMessage: function () {
+
+		var element = document.createElement( 'div' );
+		element.id = 'webgl-error-message';
+		element.style.fontFamily = 'monospace';
+		element.style.fontSize = '13px';
+		element.style.fontWeight = 'normal';
+		element.style.textAlign = 'center';
+		element.style.background = '#fff';
+		element.style.color = '#000';
+		element.style.padding = '1.5em';
+		element.style.width = '400px';
+		element.style.margin = '5em auto 0';
+
+		if ( ! this.webgl ) {
+
+			element.innerHTML = window.WebGLRenderingContext ? [
+				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' ) : [
+				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' );
+
+		}
+
+		return element;
+
+	},
+
+	addGetWebGLMessage: function ( parameters ) {
+
+		var parent, id, element;
+
+		parameters = parameters || {};
+
+		parent = parameters.parent !== undefined ? parameters.parent : document.body;
+		id = parameters.id !== undefined ? parameters.id : 'oldie';
+
+		element = Detector.getWebGLErrorMessage();
+		element.id = id;
+
+		parent.appendChild( element );
+
+	}
+
+};
+
+
+
+
+
+THREE.OrbitControls = function ( object, domElement ) {
+
+	this.object = object;
+	this.domElement = ( domElement !== undefined ) ? domElement : document;
+
+	// API
+
+	this.enabled = true;
+
+	this.center = new THREE.Vector3();
+
+	this.userZoom = true;
+	this.userZoomSpeed = 1.0;
+
+	this.userRotate = true;
+	this.userRotateSpeed = 1.0;
+
+	this.userPan = true;
+	this.userPanSpeed = 2.0;
+
+	this.autoRotate = false;
+	this.autoRotateSpeed = 2.0; // 30 seconds per round when fps is 60
+
+	this.minPolarAngle = 0; // radians
+	this.maxPolarAngle = Math.PI; // radians
+
+	this.minDistance = 0;
+	this.maxDistance = Infinity;
+
+	// 65 /*A*/, 83 /*S*/, 68 /*D*/
+	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40, ROTATE: 65, ZOOM: 83, PAN: 68 };
+
+	// internals
+
+	var scope = this;
+
+	var EPS = 0.000001;
+	var PIXELS_PER_ROUND = 1800;
+
+	var rotateStart = new THREE.Vector2();
+	var rotateEnd = new THREE.Vector2();
+	var rotateDelta = new THREE.Vector2();
+
+	var zoomStart = new THREE.Vector2();
+	var zoomEnd = new THREE.Vector2();
+	var zoomDelta = new THREE.Vector2();
+
+	var phiDelta = 0;
+	var thetaDelta = 0;
+	var scale = 1;
+
+	var lastPosition = new THREE.Vector3();
+
+	var STATE = { NONE: -1, ROTATE: 0, ZOOM: 1, PAN: 2 };
+	var state = STATE.NONE;
+
+	// events
+
+	var changeEvent = { type: 'change' };
+
+
+	this.rotateLeft = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		thetaDelta -= angle;
+
+	};
+
+	this.rotateRight = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		thetaDelta += angle;
+
+	};
+
+	this.rotateUp = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		phiDelta -= angle;
+
+	};
+
+	this.rotateDown = function ( angle ) {
+
+		if ( angle === undefined ) {
+
+			angle = getAutoRotationAngle();
+
+		}
+
+		phiDelta += angle;
+
+	};
+
+	this.zoomIn = function ( zoomScale ) {
+
+		if ( zoomScale === undefined ) {
+
+			zoomScale = getZoomScale();
+
+		}
+
+		scale /= zoomScale;
+
+	};
+
+	this.zoomOut = function ( zoomScale ) {
+
+		if ( zoomScale === undefined ) {
+
+			zoomScale = getZoomScale();
+
+		}
+
+		scale *= zoomScale;
+
+	};
+
+	this.pan = function ( distance ) {
+
+		distance.transformDirection( this.object.matrix );
+		distance.multiplyScalar( scope.userPanSpeed );
+
+		this.object.position.add( distance );
+		this.center.add( distance );
+
+	};
+
+	this.update = function () {
+
+		var position = this.object.position;
+		var offset = position.clone().sub( this.center );
+
+		// angle from z-axis around y-axis
+
+		var theta = Math.atan2( offset.x, offset.z );
+
+		// angle from y-axis
+
+		var phi = Math.atan2( Math.sqrt( offset.x * offset.x + offset.z * offset.z ), offset.y );
+
+		if ( this.autoRotate ) {
+
+			this.rotateLeft( getAutoRotationAngle() );
+
+		}
+
+		theta += thetaDelta;
+		phi += phiDelta;
+
+		// restrict phi to be between desired limits
+		phi = Math.max( this.minPolarAngle, Math.min( this.maxPolarAngle, phi ) );
+
+		// restrict phi to be betwee EPS and PI-EPS
+		phi = Math.max( EPS, Math.min( Math.PI - EPS, phi ) );
+
+		var radius = offset.length() * scale;
+
+		// restrict radius to be between desired limits
+		radius = Math.max( this.minDistance, Math.min( this.maxDistance, radius ) );
+
+		offset.x = radius * Math.sin( phi ) * Math.sin( theta );
+		offset.y = radius * Math.cos( phi );
+		offset.z = radius * Math.sin( phi ) * Math.cos( theta );
+
+		position.copy( this.center ).add( offset );
+
+		this.object.lookAt( this.center );
+
+		thetaDelta = 0;
+		phiDelta = 0;
+		scale = 1;
+
+		if ( lastPosition.distanceTo( this.object.position ) > 0 ) {
+
+			this.dispatchEvent( changeEvent );
+
+			lastPosition.copy( this.object.position );
+
+		}
+
+	};
+
+
+	function getAutoRotationAngle() {
+
+		return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
+
+	}
+
+	function getZoomScale() {
+
+		return Math.pow( 0.95, scope.userZoomSpeed );
+
+	}
+
+	function onMouseDown( event ) {
+
+		if ( scope.enabled === false ) return;
+		if ( scope.userRotate === false ) return;
+
+		event.preventDefault();
+
+		if ( state === STATE.NONE )
+		{
+			if ( event.button === 0 )
+				state = STATE.ROTATE;
+			if ( event.button === 1 )
+				state = STATE.ZOOM;
+			if ( event.button === 2 )
+				state = STATE.PAN;
+		}
+		
+		
+		if ( state === STATE.ROTATE ) {
+
+			//state = STATE.ROTATE;
+
+			rotateStart.set( event.clientX, event.clientY );
+
+		} else if ( state === STATE.ZOOM ) {
+
+			//state = STATE.ZOOM;
+
+			zoomStart.set( event.clientX, event.clientY );
+
+		} else if ( state === STATE.PAN ) {
+
+			//state = STATE.PAN;
+
+		}
+
+		document.addEventListener( 'mousemove', onMouseMove, false );
+		document.addEventListener( 'mouseup', onMouseUp, false );
+
+	}
+
+	function onMouseMove( event ) {
+
+		if ( scope.enabled === false ) return;
+
+		event.preventDefault();
+
+		
+		
+		if ( state === STATE.ROTATE ) {
+
+			rotateEnd.set( event.clientX, event.clientY );
+			rotateDelta.subVectors( rotateEnd, rotateStart );
+
+			scope.rotateLeft( 2 * Math.PI * rotateDelta.x / PIXELS_PER_ROUND * scope.userRotateSpeed );
+			scope.rotateUp( 2 * Math.PI * rotateDelta.y / PIXELS_PER_ROUND * scope.userRotateSpeed );
+
+			rotateStart.copy( rotateEnd );
+
+		} else if ( state === STATE.ZOOM ) {
+
+			zoomEnd.set( event.clientX, event.clientY );
+			zoomDelta.subVectors( zoomEnd, zoomStart );
+
+			if ( zoomDelta.y > 0 ) {
+
+				scope.zoomIn();
+
+			} else {
+
+				scope.zoomOut();
+
+			}
+
+			zoomStart.copy( zoomEnd );
+
+		} else if ( state === STATE.PAN ) {
+
+			var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+			var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+			scope.pan( new THREE.Vector3( - movementX, movementY, 0 ) );
+
+		}
+
+	}
+
+	function onMouseUp( event ) {
+
+		if ( scope.enabled === false ) return;
+		if ( scope.userRotate === false ) return;
+
+		document.removeEventListener( 'mousemove', onMouseMove, false );
+		document.removeEventListener( 'mouseup', onMouseUp, false );
+
+		state = STATE.NONE;
+
+	}
+
+	function onMouseWheel( event ) {
+
+		if ( scope.enabled === false ) return;
+		if ( scope.userZoom === false ) return;
+
+		var delta = 0;
+
+		if ( event.wheelDelta ) { // WebKit / Opera / Explorer 9
+
+			delta = event.wheelDelta;
+
+		} else if ( event.detail ) { // Firefox
+
+			delta = - event.detail;
+
+		}
+
+		if ( delta > 0 ) {
+
+			scope.zoomOut();
+
+		} else {
+
+			scope.zoomIn();
+
+		}
+
+	}
+
+	function onKeyDown( event ) {
+
+		if ( scope.enabled === false ) return;
+		if ( scope.userPan === false ) return;
+
+		switch ( event.keyCode ) {
+
+			/*case scope.keys.UP:
+				scope.pan( new THREE.Vector3( 0, 1, 0 ) );
+				break;
+			case scope.keys.BOTTOM:
+				scope.pan( new THREE.Vector3( 0, - 1, 0 ) );
+				break;
+			case scope.keys.LEFT:
+				scope.pan( new THREE.Vector3( - 1, 0, 0 ) );
+				break;
+			case scope.keys.RIGHT:
+				scope.pan( new THREE.Vector3( 1, 0, 0 ) );
+				break;
+			*/
+			case scope.keys.ROTATE:
+				state = STATE.ROTATE;
+				break;
+			case scope.keys.ZOOM:
+				state = STATE.ZOOM;
+				break;
+			case scope.keys.PAN:
+				state = STATE.PAN;
+				break;
+				
+		}
+
+	}
+	
+	function onKeyUp( event ) {
+
+		switch ( event.keyCode ) {
+
+			case scope.keys.ROTATE:
+			case scope.keys.ZOOM:
+			case scope.keys.PAN:
+				state = STATE.NONE;
+				break;
+		}
+
+	}
+
+	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	this.domElement.addEventListener( 'mousedown', onMouseDown, false );
+	this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
+	this.domElement.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
+	window.addEventListener( 'keydown', onKeyDown, false );
+	window.addEventListener( 'keyup', onKeyUp, false );
+
+};
+
+THREE.OrbitControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+
+
+
+
+
+var container, scene, camera, renderer, controls;
+var keyboard = new THREEx.KeyboardState();
+var clock = new THREE.Clock;
+
+var movingCube;
+var collideMeshList = [];
+var cubes = [];
+var message = document.getElementById("message");
+var crash = false;
+var score = 0;
+var scoreText = document.getElementById("score");
+var id = 0;
+var crashId = " ";
+var lastCrashId = " ";
+
+init();
+animate();
+
+function init() {
+    // Scene
+    scene = new THREE.Scene();
+    // Camera
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    camera = new THREE.PerspectiveCamera(45, screenWidth / screenHeight, 1, 20000);
+    camera.position.set(0, 170, 400);
+
+    // Renderer
+    if (Detector.webgl) {
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+    } else {
+        renderer = new THREE.CanvasRenderer();
+    }
+    renderer.setSize(screenWidth * 0.85, screenHeight * 0.85);
+    container = document.getElementById("ThreeJS");
+    container.appendChild(renderer.domElement);
+
+    THREEx.WindowResize(renderer, camera);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+   
+    geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(-250, -1, -3000));
+    geometry.vertices.push(new THREE.Vector3(-300, -1, 200));
+    material = new THREE.LineBasicMaterial({
+        color: 0x6699FF, linewidth: 5, fog: true
+    });
+    var line1 = new THREE.Line(geometry, material);
+    scene.add(line1);
+    geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(250, -1, -3000));
+    geometry.vertices.push(new THREE.Vector3(300, -1, 200));
+    var line2 = new THREE.Line(geometry, material);
+    scene.add(line2);
+
+
+
+    var cubeGeometry = new THREE.CubeGeometry(50, 25, 60, 5, 5, 5);
+    var wireMaterial = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true
+    });
+
+
+    movingCube = new THREE.Mesh(cubeGeometry, wireMaterial);
+    //            movingCube = new THREE.Mesh(cubeGeometry, material);
+    //            movingCube = new THREE.BoxHelper(movingCube);
+    movingCube.position.set(0, 25, -20);
+    scene.add(movingCube);
+
+
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    update();
+    renderer.render(scene, camera);
+
+}
+
+function update() {
+    var delta = clock.getDelta();
+    var moveDistance = 200 * delta;
+    //console.log(moveDistance);
+    var rotateAngle = Math.PI / 2 * delta;
+
+    //            if (keyboard.pressed("A")) {
+    //                camera.rotation.z -= 0.2 * Math.PI / 180;
+    //                console.log("press A")
+    //            }
+    //            if (keyboard.pressed("D")) {
+    //                movingCube.rotation.y += rotateAngle;
+    //            }
+
+    if (keyboard.pressed("left") || keyboard.pressed("A")) {
+        if (movingCube.position.x > -270)
+            movingCube.position.x -= moveDistance;
+        if (camera.position.x > -150) {
+            camera.position.x -= moveDistance * 0.6;
+            if (camera.rotation.z > -5 * Math.PI / 180) {
+                camera.rotation.z -= 0.2 * Math.PI / 180;
+            }
+        }
+    }
+    if (keyboard.pressed("right") || keyboard.pressed("D")) {
+        if (movingCube.position.x < 270)
+            movingCube.position.x += moveDistance;
+        if (camera.position.x < 150) {
+            camera.position.x += moveDistance * 0.6;
+            if (camera.rotation.z < 5 * Math.PI / 180) {
+                camera.rotation.z += 0.2 * Math.PI / 180;
+            }
+        }
+    }
+    if (keyboard.pressed("up") || keyboard.pressed("W")) {
+        movingCube.position.z -= moveDistance;
+    }
+    if (keyboard.pressed("down") || keyboard.pressed("S")) {
+        movingCube.position.z += moveDistance;
+    }
+
+    if (!(keyboard.pressed("left") || keyboard.pressed("right") ||
+        keyboard.pressed("A") || keyboard.pressed("D"))) {
+        delta = camera.rotation.z;
+        camera.rotation.z -= delta / 10;
+    }
+
+
+    var originPoint = movingCube.position.clone();
+
+    for (var vertexIndex = 0; vertexIndex < movingCube.geometry.vertices.length; vertexIndex++) {
+       
+        var localVertex = movingCube.geometry.vertices[vertexIndex].clone();
+       
+        var globalVertex = localVertex.applyMatrix4(movingCube.matrix);
+        var directionVector = globalVertex.sub(movingCube.position);
+
+        var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+        var collisionResults = ray.intersectObjects(collideMeshList);
+        if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
+            crash = true;
+            crashId = collisionResults[0].object.name;
+            break;
+        }
+        crash = false;
+    }
+
+    if (crash) {
+        //            message.innerText = "crash";
+        movingCube.material.color.setHex(0x346386);
+        console.log("Crash");
+        if (crashId !== lastCrashId) {
+            score -= 100;
+            lastCrashId = crashId;
+        }
+
+        document.getElementById('explode_sound').play()
+    } else {
+        //            message.innerText = "Safe";
+        movingCube.material.color.setHex(0x00ff00);
+    }
+
+    if (Math.random() < 0.03 && cubes.length < 30) {
+        makeRandomCube();
+    }
+
+    for (i = 0; i < cubes.length; i++) {
+        if (cubes[i].position.z > camera.position.z) {
+            scene.remove(cubes[i]);
+            cubes.splice(i, 1);
+            collideMeshList.splice(i, 1);
+        } else {
+            cubes[i].position.z += 10;
+        }
+        //                renderer.render(scene, camera);
+    }
+
+    score += 0.1;
+    scoreText.innerText = "Score:" + Math.floor(score);
+
+    //controls.update();
+}
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function makeRandomCube() {
+    var a = 1 * 50,
+        b = getRandomInt(1, 3) * 50,
+        c = 1 * 50;
+    var geometry = new THREE.CubeGeometry(a, b, c);
+    var material = new THREE.MeshBasicMaterial({
+        color: Math.random() * 0xffffff,
+        size: 3
+    });
+
+
+    var object = new THREE.Mesh(geometry, material);
+    var box = new THREE.BoxHelper(object);
+    //            box.material.color.setHex(Math.random() * 0xffffff);
+    box.material.color.setHex(0xff0000);
+
+    box.position.x = getRandomArbitrary(-250, 250);
+    box.position.y = 1 + b / 2;
+    box.position.z = getRandomArbitrary(-800, -1200);
+    cubes.push(box);
+    box.name = "box_" + id;
+    id++;
+    collideMeshList.push(box);
+
+    scene.add(box);
+}
+
+
+
+
